df7290f8b38af79cc34592c561d4c69674c52ff0
Luis Zamora <bico83@gmail.com>
2021-12-05 16:35:06 -0600

    Initial version

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# manga_downloader
+# Basic script to download MANGAS
+This script will download images and store them by chapters from a KissManga link
+with the name of your favorite manga.
+Check the already tested samples and replace source and destination folder by yours.
+
+## Installation
+
+```
+pip install virtualenv
+
+python -m virtualenv venv --python=python3.8
+
+source venv/bin/activate
+
+pip install bs4
+pip install requests
+```
+
+
+## Finding your favorite mangas
+1. Open the manga_creator.py file
+2. Go to the main section
+3. Replace the source parameter by the link of your favorite manga in http://kissmanga.nl
+4. Replace the destination parameter by a folder name to store the downloaded manga
+
+> ##### Downloading Collections
+> 
+> - If you want to download all the available collection, pass `full_collection` param as `True`
+> - if you don'n need the full collection, pass `full_collection` param as `False` and pass the link or list of links you want to download to the source param
+
+
+## Deployment
+- Enable virtual environment
+```
+source venv/bin/activate
+```
+- Go to the folder where app.py is located
+- Execute app.py
+```
+python manga_creator.py
+```
---
+++ manga_creator.py
@@ -0,0 +1,95 @@
+import os
+import re
+from bs4 import BeautifulSoup
+import requests
+
+
+def get_manga(full_collection=True, source=None, dest=None):
+    # Get the url of available chapters in a link with available chapters
+    try:
+        if full_collection:
+            chapters_url = get_url_of_available_chapters(source)
+        else:
+            if type(source) is list:
+                chapters_url = source
+            elif type(source) is str:
+                chapters_url = list()
+                chapters_url.append(source)
+            else:
+                return 'Fail', \
+                       'source should be a link to page with the list of chapters, ' \
+                       'a list with the links of chapters you want to download or, ' \
+                       'the link of the chapter you want to download'
+    except Exception as e:
+        return e, 'source was not passed or is invalid'
+
+    # Get the indexes of chapters to download
+    try:
+        chapters_ids = get_indexes_of_available_chapters(chapters_url)
+    except Exception as e:
+        return e, 'source link probably does not contain the number of chapter or page'
+
+    try:
+        # Get books and store them by book
+        get_images_per_chapter(chapters_url, chapters_ids, dest)
+    except Exception as e:
+        return e, 'destination folder was not passed or is invalid'
+
+
+def get_url_of_available_chapters(source):
+    href_class_name = source.split('/')[-1] + '-chapter'
+    req_result = requests.get(source)
+    page_content = BeautifulSoup(req_result.content, 'html.parser')
+    classes = page_content.find_all(href=re.compile(href_class_name))
+    urls = list(set([x.attrs['href'] for x in classes]))
+    return urls
+
+
+def get_indexes_of_available_chapters(available_chapters):
+    indexes = [x.split('-')[-1] for x in available_chapters]
+    return [x.split('#')[0] for x in indexes]
+
+
+def get_images_per_chapter(chapters_url, chapter_ids, dest):
+    # Match chapter_url with chapter_id in order to name folders
+    for chapter_url, chapter_id in zip(chapters_url, chapter_ids):
+        # Get urls of pages
+        req_result = requests.get(chapter_url)
+        if req_result.status_code == 200:
+            page_content = BeautifulSoup(req_result.content, 'html.parser')
+            classes = page_content.find(id='arraydata')
+        else:
+            continue
+
+        # Save images in the destination
+        urls = classes.text.split(',')
+        for id, url in enumerate(urls):
+            response = requests.get(url)
+            folder = os.path.join(dest, f'chapter_{chapter_id}')
+            file = os.path.join(folder, f'sample_{id}.jpg')
+            if not os.path.isdir(folder):
+                os.mkdir(folder)
+            if response.status_code == 200:
+                if not os.path.isfile(file):
+                    with open(file, 'wb') as f:
+                        f.write(response.content)
+
+
+if __name__ == "__main__":
+    # Download manga full collection
+    # get_manga(source=f'http://kissmanga.nl/manga/naruto-full-color',
+    #           dest=f'/Users/miguelpalos/Downloads/miguel_palos/mamadas_de_bico/')
+
+    get_manga(source=f'http://kissmanga.nl/manga/cowboy-bebop',
+              dest=f'/Users/miguelpalos/Downloads/miguel_palos/mamadas_de_bico/')
+
+    # Download a single chapter of the whole manga
+    # get_manga(full_collection=False,
+    #           source='http://kissmanga.nl/naruto-full-color-chapter-175#1',
+    #           dest=f'/Users/miguelpalos/Downloads/miguel_palos/mamadas_de_bico')
+
+    # Download a bunch of chapters
+    # get_manga(full_collection=False,
+    #           source=['http://kissmanga.nl/naruto-full-color-chapter-158',
+    #                   'http://kissmanga.nl/naruto-full-color-chapter-159'],
+    #           dest=f'/Users/miguelpalos/Downloads/miguel_palos/mamadas_de_bico')
